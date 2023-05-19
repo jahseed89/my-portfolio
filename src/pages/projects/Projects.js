e3e35cfec83d4e8448e60882236bb3acf205bc17
Samson Ocran <samson.ocran@gmail.com>
2023-05-19 09:52:53 +0100

    first commit

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+      <div>
+        <header>Featured Projects</header>
+        <p>Here are some of the works that speak in bringing idea to life</p>
+        <p>Hover on any of them to see more</p>
+      </div>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
