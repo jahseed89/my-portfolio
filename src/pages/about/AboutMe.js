41c59e5c781e0a4ef59fc530a319404de7d48126
Samson Ocran <samson.ocran@gmail.com>
2023-08-24 23:23:59 +0100

    working on responsiveness

--- src/pages/about/AboutMe.js
+++ src/pages/about/AboutMe.js
@@ -90,3 +90,88 @@ const AboutMe = () => {
 };
 
 export default AboutMe;
+// import React, { useState } from "react";
+// import './aboutMe.scss'
+
+// function TransactionTable({ txns }) {
+//   const [dateSelected, setDateSelected] = useState("2019-11-29");
+//   const [sortingOrder, setSortingOrder] = useState("asc"); // Initial sorting order
+//   const [sortedTxns, setSortedTxns] = useState(txns); // Initially display unsorted transactions
+
+//   const handelDate = (e) => {
+//     setDateSelected(e.target.value);
+//   };
+
+//   // Handling the filter click
+//   const handleFilter = () => {
+//     const filteredTnxns = txns.filter((txn) => txn.date === dateSelected);
+//     setSortedTxns(filteredTnxns); // Update sorted transactions when filtered
+//   };
+
+//   const sort = () => {
+//     // Toggle sorting order
+//     const newSortingOrder = sortingOrder === "asc" ? "desc" : "asc";
+
+//     // Sort the transactions based on the amount and sorting order
+//     const sortedTnxns = [...sortedTxns].sort((a, b) => {
+//       return newSortingOrder === "asc"
+//         ? a.amount - b.amount
+//         : b.amount - a.amount;
+//     });
+
+//     setSortingOrder(newSortingOrder);
+//     setSortedTxns(sortedTnxns);
+//   };
+
+//   return (
+//     <div className="layout-column align-items-center mt-50">
+//       <section className="layout-row align-items-center justify-content-center">
+//         <label className="mr-10">Transaction Date</label>
+//         <input
+//           id="date"
+//           type="date"
+//           defaultValue="2019-11-29"
+//           value={dateSelected}
+//           onChange={handelDate}
+//         />
+//         <button className="small" onClick={handleFilter}>
+//           Filter
+//         </button>
+//       </section>
+
+//       <div className="card mt-50">
+//         <table className="table">
+//           <thead>
+//             <tr className="table">
+//               <th className="table-header">Date</th>
+//               <th className="table-header">Description</th>
+//               <th className="table-header">Type</th>
+//               <th className="table-header">
+//                 <span id="amount" onClick={sort}>
+//                   Amount ($)
+//                 </span>
+//               </th>
+//               <th className="table-header">Available Balance</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {sortedTxns.map((txn) => {
+//               return (
+//                 <tr key={txn.id}>
+//                   <td>{txn.date}</td>
+//                   <td>{txn.description}</td>
+//                   <td>{txn.type === 1 ? 'Debit' : 'Credit'}</td>
+//                   <td>{txn.amount}</td>
+//                   <td>{txn.balance}</td>
+
+//                 </tr>
+//               )
+//             })}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default TransactionTable;
